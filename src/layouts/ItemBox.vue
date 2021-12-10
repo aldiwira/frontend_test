fca7b6fd444f8152298b7a1a83b0454076e5e141
aldiwira <aldiwira.aw@gmail.com>
2021-12-10 21:05:27 +0700

    add home page

---
+++ src/layouts/ItemBox.vue
@@ -0,0 +1,22 @@
+<template>
+    <router-link :to="`/detail/${product.id}`">
+        <a href="#" class="group">
+            <div class="w-full h-52 aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
+                <img :src="product.images[0]" class="w-full h-full object-center object-cover group-hover:opacity-75">
+            </div>
+            <h3 class="mt-4 text-sm text-gray-700">
+            {{product.title}}
+            </h3>
+            <p class="mt-1 text-lg font-medium text-gray-900">
+            {{product.price}}
+            </p>
+        </a>
+    </router-link>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    props: ['product']
+})
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,24 +1,29 @@
 <template>
-  <main>
-    <section>
-      <article
-        v-for="product in products"
-        :key="product.id"
-      >
-        <h3>
-          <router-link :to="`/detail/${product.id}`">{{ product.title }}</router-link>
-        </h3>
-        <p>Rp. {{ product.price }}</p>
-      </article>
-    </section>
+
+  <main class="container mx-auto">
+    <div class="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
+        <h2 class="sr-only">Products</h2>
+        <div class="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+          <article
+            v-for="product in products"
+            :key="product.id"
+          >
+            <ItemBox :product="product"/>
+          </article>
+        </div>
+    </div>
   </main>
 </template>
 
 <script>
 import { defineComponent } from 'vue';
 import mockData from '../mock-data.json';
+import ItemBox from "../layouts/ItemBox.vue";
 
 export default defineComponent({
+  components: {
+    ItemBox
+  },
   data() {
     return {
       products: mockData,
